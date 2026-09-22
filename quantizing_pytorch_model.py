88a0ad427fe83265ad40809ff11e2bbc30096071
QuantaX <116838749+Fauaaz@users.noreply.github.com>
2026-09-22 16:54:25 +0100

    quantization of the model for mobile inference(onnx, int8)

---
+++ quantizing_pytorch_model.py
@@ -0,0 +1,41 @@
+import torch
+import onnx
+import onnxscript
+from onnxruntime.quantization import quantize_dynamic, QuantType
+from onnxruntime.quantization.shape_inference import quant_pre_process
+
+
+model = torch.load('fine_tuned_model.pth', map_location=torch.device('cpu'), weights_only=False)
+model.eval()
+
+dummy_input = torch.randn(1, 3, 224, 224)
+
+torch.onnx.export(
+    model,
+    dummy_input,
+    "fine_tuned_model_fp32.onnx",
+    export_params=True,
+    opset_version=18,
+    do_constant_folding=True,
+    input_names=['input'],
+    output_names=['output'],
+    dynamic_axes={'input': {0: 'batch_size'}, 'output': {0: 'batch_size'}},
+)
+
+input_fp32 = "fine_tuned_model_fp32.onnx"
+input_preprocessed = "fine_tuned_model_preprocessed.onnx"
+output_int8 = "fine_tuned_model_int8.onnx"
+
+# 1. Fix shape mismatches inside the ONNX model graph
+quant_pre_process(
+    input_model_path=input_fp32,
+    output_model_path=input_preprocessed,
+    skip_symbolic_shape_inference=False,
+)
+
+# 2. Quantize the pre-processed model
+quantize_dynamic(
+    model_input=input_preprocessed,
+    model_output=output_int8,
+    weight_type=QuantType.QUInt8,
+)
--- test.py
+++ test.py
@@ -13,7 +13,7 @@ from PIL import Image
 from tempfile import TemporaryDirectory
 from pathlib import Path
 
-dataset_path = Path('C:\\Users\\USER\\Desktop\\passport_classifier\\cleaned_dataset\\test')
+dataset_path = Path('"C:\\Users\\USER\\Desktop\\pic_dataset\\reject\\00E681E8-F05F-4703-A0B4-2900613F85A7 - Copy.png"')
 
 data_transforms = {
     'test': transforms.Compose([
@@ -30,9 +30,15 @@ image_datasets = {
         data_transforms["test"]
     )
 }
-                 
+
+
+image = Image.open(dataset_path).convert("RGB")
+
+image_tensor = data_transforms["test"](image)
+image_tensor = image_tensor.unsqueeze(0)
+
 dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=2,
-                                             shuffle=True, num_workers=2)
+                                             shuffle=True, num_workers=0)
               for x in ['test']}
 dataset_sizes = {x: len(image_datasets[x]) for x in ['test']}
 class_names = image_datasets['test'].classes
@@ -48,7 +54,7 @@ def imshow(inp, title=None):
     if title is not None:
         plt.title(title)
     plt.pause(0.001)  # pause a bit so that plots are updated
-
+    plt.show(block=True)
 
 # Get a batch of testing data
 inputs, classes = next(iter(dataloaders['test']))
@@ -56,7 +62,7 @@ inputs, classes = next(iter(dataloaders['test']))
 # Make a grid from batch
 out = torchvision.utils.make_grid(inputs)
 
-imshow(out, title=[class_names[x] for x in classes])
+imshow()
 
 model = torch.load(
     "fine_tuned_model.pth",
